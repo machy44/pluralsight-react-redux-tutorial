fa129f77628346be70f9d72e6a19220f088c153a
machy44 <machy44@gmail.com>
2018-04-01 19:29:01 +0200

    api mock , thunk calls, created CreateList and CourseListRow components

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import { render } from "react-dom";
 import configureStore from "./store/configureStore";
 import { Provider } from "react-redux";
+import { loadCourses } from "./actions/courseActions";
 import CoursesPage from "./components/coursesPage";
 
 const store = configureStore();
+store.dispatch(loadCourses());
+
 const App = () => (
   <div>
     <CoursesPage />
