a5847089bee751b33fc4c4dd442ca25b927454ce
machy44 <machy44@gmail.com>
2018-08-04 00:54:31 +0200

    watched and implemented 03. Testing action creators in 13. Testing Redux

--- src/components/course/manageCoursePage.js
+++ src/components/course/manageCoursePage.js
@@ -5,6 +5,7 @@ import { bindActionCreators } from "redux";
 import * as courseActions from "../../actions/courseActions";
 import CourseForm from "./courseForm";
 import toastr from "toastr";
+import {authorsFormattedForDropdown} from '../../selectors/selectors';
 
 export class ManageCoursePage extends Component {
   constructor(props, context) {
@@ -34,8 +35,25 @@ export class ManageCoursePage extends Component {
     return this.setState({ course });
   }
 
+  courseFormIsValid() {
+    let formIsValid = true;
+    let errors = {};
+
+    if (this.state.course.title.length < 5) {
+      errors.title = 'Title must be at least 5 characters.';
+      formIsValid = false;
+    }
+    this.setState({errors: errors});
+    return formIsValid;
+  }
+
   saveCourse(event) {
     event.preventDefault();
+
+    if(!this.courseFormIsValid()) {
+      return;
+    }
+
     this.setState({ saving: true })
     this.props.actions.saveCourse(this.state.course)
       .then(() => this.redirect())
@@ -97,16 +115,10 @@ function mapStateToProps(state, ownProps) {
     course = getCourseById(state.courses, courseId);
   }
 
-  const authorsFormattedForDropdown = state.authors.map(author => {
-    return {
-      value: author.id,
-      text: author.firstName + " " + author.lastName
-    };
-  });
 
   return {
     course,
-    authors: authorsFormattedForDropdown
+    authors: authorsFormattedForDropdown(state.authors)
   };
 }
 
--- src/components/course/manageCoursePage.test.js
+++ src/components/course/manageCoursePage.test.js
@@ -5,6 +5,22 @@ import { ManageCoursePage } from './manageCoursePage';
 
 describe('Manage Course Page', () => {
   it('sets error message when trying to save empty title', () => {
-    const wrapper = mount(<ManageCoursePage/>);
+    const props = {
+      authors: [],
+      actions: { saveCourse: () => { return Promise.resolve(); }},
+      course: {
+        id: '',
+        watchHref: '',
+        title: '',
+        authorId: '',
+        length: '',
+        category: ''
+      },
+    };
+  const wrapper = mount(<ManageCoursePage { ...props }/>);
+    const saveButton = wrapper.find('input').last();
+    expect(saveButton.prop('type')).toBe('submit');
+    saveButton.simulate('click');
+    expect(wrapper.state().errors.title).toBe('Title must be at least 5 characters.')
   });
 });
